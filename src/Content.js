7c396a3e8de279ca44dbd1bc0de561df07c730ca
JornaJan <loverong09@126.com>
2022-11-07 22:24:41 +0800

    Click Events

--- src/Content.js
+++ src/Content.js
@@ -6,12 +6,28 @@ const Content = () => {
     const init = Math.floor(Math.random() * 3)
     return names[init]
   }
+
+  const handleClick = () => {
+    console.log('You clicked it!')
+  }
+
+  const handleClick2 = () => {
+    console.log(`${name} You clicked it!`)
+  }
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText)
+  }
+
   return (
     <main>
-      <h1>
+      <h1 onDoubleClick={handleClick}>
         Hello {handleNameChange()}
       </h1>
       <p>{name}</p>
+      <button onClick={handleClick}>Click It</button>
+      <button onClick={() => handleClick2('Jorna')}>Click It</button>
+      <button onClick={(e) => handleClick3(e)}>Click It</button>
     </main>
   )
 }
