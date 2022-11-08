915eabbea3d8a66b7c393349522cd11663448590
JornaJan <loverong09@126.com>
2022-11-08 20:58:15 +0800

    useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import Header from './Header'
 import Content from './Content'
 import Footer from './Footer'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import AddItem from './AddItem'
 import SearchItem from './SearchItem'
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist'))) // 默认值
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []) // 默认值
 
   const [newItem, setNewItem] = useState('')
   const [search, setSearch] = useState('')
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems)
-    localStorage.setItem('shoppinglist', JSON.stringify(newItems))
-  }
+  console.log('before use effect')
+  useEffect(() => {
+    console.log('inner use effect')
+    localStorage.setItem('shoppinglist', JSON.stringify(items))
+  }, [items])
+  console.log('after use effect')
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1
@@ -22,7 +24,7 @@ function App() {
     const listItems = [...items, myNewItem]
 
     console.log(listItems)
-    setAndSaveItems(listItems)
+    setItems(listItems)
   }
 
   const handleSubmit = (e) => {
@@ -35,30 +37,46 @@ function App() {
 
   const handleCheck = (id) => {
     //console.log(id)
-    const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked}: item)
-    setAndSaveItems(listItems)
+    const listItems = items.map(
+      (item) => item.id === id ? 
+        {...item, checked: !item.checked} :
+        item
+      )
+    setItems(listItems)
   }
 
   const handleDelete = (id) => {
     console.log(id)
     const listItems = items.filter(item => item.id !== id)
-    setAndSaveItems(listItems)
-
+    setItems(listItems)
   }
 
   return (
     <div className="App">
-      <Header title="Jorna Jan" />
-      <SearchItem search={search} setSearch={setSearch} />
-      <AddItem newItem={newItem} setNewItem={setNewItem} handleSubmit={handleSubmit} />
+      <Header 
+        title="Jorna Jan" 
+      />
+      <SearchItem 
+        search={search} 
+        setSearch={setSearch}
+      />
+      <AddItem 
+        newItem={newItem} 
+        setNewItem={setNewItem} 
+        handleSubmit={handleSubmit} 
+      />
       <Content 
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}  
+        items={items.filter(item => (
+          (item.item).toLowerCase()).includes(search.toLowerCase())
+        )}  
         handleCheck={handleCheck} 
         handleDelete={handleDelete} 
       />
-      <Footer length={items.length} />
+      <Footer 
+        length={items.length} 
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
