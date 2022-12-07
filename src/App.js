0cd7b075d8e7d74cb4494419c906804ee1170cb9
JornaJan <loverong09@126.com>
2022-12-07 17:41:05 +0800

    counter demo hooks | useEffect & useLayoutEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useEffect, useLayoutEffect } from 'react'
 
 function App() {
+  const [number, setNumber] = useState(0)
+  const [sectionStyle, setSectionStyle] = useState({})
+  const sectionRef = useRef()
+
+  useLayoutEffect(() => {
+    const random = Math.floor(Math.random() * 500)
+
+    for (let i = 0; i <= 100000000; i++) {
+      if (i === 100000000) setSectionStyle({paddingTop: `${random}px`})
+    }
+  }, [number])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter</h1> 
+      <section ref={sectionRef} style={sectionStyle}>
+        <p>{number}</p>
+        <div>
+          <button onClick={() => setNumber(prev => prev - 1)}>-</button>
+          <button onClick={() => setNumber(prev => prev + 1)}>+</button>
+        </div>
+      </section>
     </div>
   );
 }
